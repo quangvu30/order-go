fa39e4fd73795e873388cab342f868a2d162c2c1
quangvu30 <docnhatvonhi40@gmail.com>
2024-01-15 14:25:42 +0700

    format

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -14,14 +14,14 @@ import (
 )
 
 func main() {
-	logging, err := logger.NewLogger()
+	_, err := logger.NewLogger()
 	if err != nil {
 		log.Fatalf("Error creating logger: %v", err)
 		return
 	}
 	config, err := config.GetConfig()
 	if err != nil {
-		logging.Error("Failed to get config", err)
+		logger.Log.Error("Failed to get config", err.Error())
 		return
 	}
 	mongoDatabase := mongo.Connect()
@@ -29,7 +29,7 @@ func main() {
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(config.GrpcAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		logging.Error("did not connect: ", err)
+		logger.Log.Error("did not connect: ", err.Error())
 		return
 	}
 	defer conn.Close()
@@ -37,13 +37,13 @@ func main() {
 	gin.SetMode(config.GinMode)
 	engine, err := routes.InitRouter(mongoDatabase, conn, config)
 	if err != nil {
-		logging.Error("Failed to init gin", err)
+		logger.Log.Error("Failed to init gin", err.Error())
 		return
 	}
 
-	logging.Info("Starting server at http://localhost:", config.ListenPort)
+	logger.Log.Info("Starting server at http://localhost:", config.ListenPort)
 	e := engine.Run(":" + config.ListenPort)
 	if e != nil {
-		logging.Error("Failed to run server", e)
+		logger.Log.Error("Failed to run server", e.Error())
 	}
 }
